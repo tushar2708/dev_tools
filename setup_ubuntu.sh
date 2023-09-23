653540b2d173240798ea5e983bac3bf326795faa
Tushar Dwivedi <tdwivedi2708@gmail.com>
2023-09-23 09:50:47 +0530

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -2,3 +2,7 @@ sudo apt update
 sudo apt install curl
 sudo apt install git
 sudo apt install dkms
+
+sudo apt-get install terminator      #  A sensible terminal app
+sudo apt-get install autokey-gtk     #  A script a day, keeps the boredom away
+sudo apt install git python3 build-essential php-cli php-curl rename docker.io
