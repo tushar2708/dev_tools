c75810c7c4bb3037173e176c6892adf245990f50
Tushar Dwivedi <tdwivedi2708@gmail.com>
2023-09-23 10:21:17 +0530

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -3,6 +3,7 @@ sudo apt install curl
 sudo apt install git
 sudo apt install dkms
 
-sudo apt-get install terminator      #  A sensible terminal app
+#sudo apt-get install terminator     # A reasonable terminal app
+sudo apt-get install tilix           # A terminal app, with tabs and pans (iTerm2 alternative)
 sudo apt-get install autokey-gtk     #  A script a day, keeps the boredom away
 sudo apt install git python3 build-essential php-cli php-curl rename docker.io
