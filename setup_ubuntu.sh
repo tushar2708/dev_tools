5f0bd114f19eac263f55456163ff027625d8f6bf
Tushar Dwivedi <tdwivedi2708@gmail.com>
2024-11-03 20:59:51 +0530

    Added some tools to install

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -3,7 +3,18 @@ sudo apt install curl
 sudo apt install git
 sudo apt install dkms
 
-#sudo apt-get install terminator     # A reasonable terminal app
 sudo apt-get install tilix           # A terminal app, with tabs and pans (iTerm2 alternative)
 sudo apt-get install autokey-gtk     #  A script a day, keeps the boredom away
 sudo apt install git python3 build-essential php-cli php-curl rename docker.io
+
+# Text editors and IDEs
+sudo snap install xdman          # Extreme download manager
+sudo snap install sublime-text --classic         # Install Pycharm community edition
+sudo snap install --classic code
+sudo snap install pycharm-community --classic         # Install Pycharm community edition
+sudo snap install intellij-idea-community --classic  # Install Intellij community edition
+
+
+# Productivity tools
+sudo snap install xdman          # Extreme download manager
+sudo snap install todoist        # Todoist app to manage tasks
